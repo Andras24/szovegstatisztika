9514b706191bb36d39af45d5984daf7607dc1bdd
hegea24@gmail.com <hegea24@gmail.com>
2025-05-11 23:27:16 +0200

    Create TextStatsController.cs

---
+++ SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS/BACKEND/Controllers/TextStatsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS.Data;
+using SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS.BACKEND.Models;
+
+namespace SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS.BACKEND.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TextStatsController : ControllerBase
+    {
+        private readonly ITextStatsService service;
+
+        public TextStatsController(ITextStatsService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        public ActionResult<TextStatsResult> Analyze([FromBody] TextStatsInput input)
+        {
+            var result = service.Analyze(input);
+            return Ok(result);
+        }
+    }
+}
