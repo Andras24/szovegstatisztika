8665a8e23d58687419c7e79712cb0afddc29b063
hegea24@gmail.com <hegea24@gmail.com>
2025-05-11 21:20:53 +0200

    Add project files.

---
+++ SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS/Program.cs
@@ -0,0 +1,15 @@
+namespace SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
