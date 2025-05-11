995d1797da5d992c7276d4cb4a11a69480080cdf
hegea24@gmail.com <hegea24@gmail.com>
2025-05-11 23:30:46 +0200

    coding TextStatsResult.cs

--- SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS/BACKEND/Models/TextStatsResult.cs
+++ SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS/BACKEND/Models/TextStatsResult.cs
@@ -1,6 +1,15 @@
-﻿namespace SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS.BACKEND.Models
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS.BACKEND.Models
 {
     public class TextStatsResult
     {
+        public int WordCount { get; set; }
+        public int CharacterCount { get; set; }
+        public Dictionary<string, int> MostCommonWords { get; set; }
+        public double AverageSentenceLength { get; set; }
+        public double AverageWordLength { get; set; }
     }
 }
