7406bb58c9e36d6981f16d965a9ea266976ef11a
hegea24@gmail.com <hegea24@gmail.com>
2025-05-11 23:28:02 +0200

    coding ITextStatsService.cs

--- SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS/BACKEND/Data/ITextStatsService.cs
+++ SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS/BACKEND/Data/ITextStatsService.cs
@@ -1,6 +1,12 @@
-﻿namespace SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS.BACKEND.Data
+﻿using System;
+
+using SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS.BACKEND.Models;
+
+namespace SZOVEGSTATISZTIKA_GPWIDG_HEGEDUS.Data
+
 {
     public interface ITextStatsService
     {
+        TextStatsResult Analyze(TextStatsInput input);
     }
 }
